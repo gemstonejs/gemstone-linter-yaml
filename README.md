6f4ae3538fa355cd2aaddd78aeeba7703dc99645
Ralf S. Engelschall <rse@engelschall.com>
2017-04-05 11:42:56 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gemstone-linter-yaml
-Linter for YAML
+
+<img src="https://rawgit.com/gemstonejs/gemstone-artwork/master/gemstone-logo-white.svg" width="300" align="right" alt=""/>
+
+Gemstone Linter YAML
+====================
+
+<p/>
+<img src="https://nodei.co/npm/gemstone-linter-yaml.png?downloads=true&stars=true" alt=""/>
+<p/>
+<img src="https://david-dm.org/rse/gemstone-linter-yaml.png" alt=""/>
+
+About
+-----
+
+This is the Linter for YAML files in the
+[Gemstone JavaScript Technology Stack](http://gemstonejs.com).
+It is based on [js-YAML](http://nodeca.github.io/js-yaml/).
+
+Copyright &amp; License
+-----------------------
+
+Copyright &copy; 2016-2017 [GemstoneJS Project](http://gemstonejs.com)<br/>
+Licensed under [Apache License 2.0](https://spdx.org/licenses/Apache-2.0)
+
---
+++ gemstone-linter-yaml.js
@@ -0,0 +1,67 @@
+/*
+**  GemstoneJS -- Gemstone JavaScript Technology Stack
+**  Copyright (c) 2016-2017 Gemstone Project <http://gemstonejs.com>
+**  Licensed under Apache License 2.0 <https://spdx.org/licenses/Apache-2.0>
+*/
+
+/*  load external requirements  */
+const path      = require("path")
+const fs        = require("fs-promise")
+const jsYAML    = require("js-yaml")
+
+/*  exported API function  */
+module.exports = async function (filenames, opts = {}, report = { sources: {}, findings: [] }) {
+    /*  interate over all source files  */
+    let passed = true
+    if (typeof opts.progress === "function")
+        opts.progress(0.0, "linting YAML: starting")
+    for (let i = 0; i < filenames.length; i++) {
+        /*  indicate progress  */
+        if (typeof opts.progress === "function")
+            opts.progress(i / filenames.length, `linting YAML: ${filenames[i]}`)
+
+        /*  read source code  */
+        let source = await fs.readFile(filenames[i], "utf8")
+
+        /*  determine name  */
+        let name = path.relative(process.cwd(), filenames[i])
+
+        /*  lint the source code  */
+        try {
+            jsYAML.safeLoad(source, {
+                filename: name,
+                schema:   jsYAML.DEFAULT_SAFE_SCHEMA,
+                onWarning: (warning) => {
+                    report.findings.push({
+                        ctx:      "YAML",
+                        filename: name,
+                        line:     warning.mark.line,
+                        column:   warning.mark.column,
+                        message:  warning.reason,
+                        ruleProc: "jsYAML",
+                        ruleId:   "*"
+                    })
+                    passed = false
+                    report.sources[name] = source
+                }
+            })
+        }
+        catch (ex) {
+            report.findings.push({
+                ctx:      "YAML",
+                filename: name,
+                line:     ex.mark.line,
+                column:   ex.mark.column,
+                message:  ex.reason,
+                ruleProc: "jsYAML",
+                ruleId:   "*"
+            })
+            report.sources[name] = source
+            passed = false
+        }
+    }
+    if (typeof opts.progress === "function")
+        opts.progress(1.0, "")
+    return passed
+}
+
